99bff9f715f4f3dcdfd4a5c30157c1849e04c571
anbbch <155988779+anbbch@users.noreply.github.com>
2026-01-29 12:26:48 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
 
     int a = 16;
     int b = 3;
+    bool egal = (a == b);
+    bool superieur = (a > b);
 
     /* Opérateurs arithmétiques */
     printf("a = %d, b = %d\n\n", a, b);
@@ -14,4 +17,8 @@ int main() {
     printf("Reste (a %% b) = %d\n", a % b);
     printf("Addition (a + b) = %d\n", a + b);
 
+    printf("a est egal a b ? %d\n", egal);
+    printf("a est superieur a b ? %d\n", superieur);
+
+    return 0;
 
