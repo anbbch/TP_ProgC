5043ef037fd9ee7d1e54ed15737c7954e3bd4881
anbbch <155988779+anbbch@users.noreply.github.com>
2026-01-29 12:47:27 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,69 @@
+#include <stdio.h>
+
+int main() {
+
+    int num1 = 16;
+    int num2 = 3;
+    char op = '+';   // Teste avec: + - * / % & | ~
+
+    int resultat = 0;
+
+    switch (op) {
+
+        case '+':
+            resultat = num1 + num2;
+            printf("%d + %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '-':
+            resultat = num1 - num2;
+            printf("%d - %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '*':
+            resultat = num1 * num2;
+            printf("%d * %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '/':
+            if (num2 == 0) {
+                printf("Erreur : division par zero !\n");
+            } else {
+                resultat = num1 / num2;
+                printf("%d / %d = %d\n", num1, num2, resultat);
+            }
+            break;
+
+        case '%':
+            if (num2 == 0) {
+                printf("Erreur : modulo par zero !\n");
+            } else {
+                resultat = num1 % num2;
+                printf("%d %% %d = %d\n", num1, num2, resultat);
+            }
+            break;
+
+        case '&':
+            resultat = num1 & num2;
+            printf("%d & %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '|':
+            resultat = num1 | num2;
+            printf("%d | %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '~':
+            resultat = ~num1;   // ~ est unaire (1 seul nombre)
+            printf("~%d = %d\n", num1, resultat);
+            break;
+
+        default:
+            printf("Operateur non reconnu : %c\n", op);
+            break;
+    }
+
+    return 0;
+}
+
 
