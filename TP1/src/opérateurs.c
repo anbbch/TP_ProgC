26f15f0e5ea9dc747c2d0efc9accd352be55df55
anbbch <155988779+anbbch@users.noreply.github.com>
2026-01-29 11:44:11 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 16;
+    int b = 3;
+
+    /* Opérateurs arithmétiques */
+    printf("a = %d, b = %d\n\n", a, b);
+    printf("Addition (a + b) = %d\n", a + b);
+    printf("Soustration (a - b) = %d\n", a - b);
+    printf("Multiplication (a x b) = %d\n", a * b);
+    printf("Division (a / b) = %d\n", a / b);
+    printf("Reste (a %% b) = %d\n", a % b);
+    printf("Addition (a + b) = %d\n", a + b);
 
 
